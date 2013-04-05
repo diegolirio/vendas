c91f4cae6a6ab92ab11519cc69cf3d23bcfddb1a
Diego Lirio <diegolirio.dl@gmail.com>
2013-04-05 18:07:30 -0300

    ....>

--- cliente/views.py
+++ cliente/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render_to_response
 from cliente.models import Cliente
 from cliente.models import Telefone
+from django import forms
 
-def clientes(request):
-	
-	clientes = Cliente.objects.all()
-	
+class ClienteForm(forms.ModelForm):
+
+    class Meta:
+        model = Cliente
+        #fields = ['id', 'cnpj_cpf', 'telefone.ddd/numero']
+
+def clientes(request):	
+	clientes = Cliente.objects.all()	
 	return render_to_response('clientes.html', {'clientes': clientes})	
+	
 	
 def set_cliente(request):
 	return render_to_response('cad_cliente.html')	
+	
+	
+def cliente_edit(request, pk=None):	
+	if request.method == 'POST':
+		form = ClienteForm(request.POST, request.FILES)  
+		if form.is_valid():
+			c = form.save(commit=False)
+			c.save()
+			
+			return clientes(request)
+	else:
+		c = Cliente.objects.get(pk=pk)
+		form = ClienteForm(instance=c)
+		return render_to_response('cliente_edit.html', {'c':c, 'form': form})		
--- vendas/urls.py
+++ vendas/urls.py
@@ -6,7 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:'),
-    url(r'^clientes$', 'cliente.views.clientes', name='clientes'),
+    url(r'^clientes$', 'cliente.views.clientes', name='clientes'),  
+     
+    #(r'^polls/(?P<poll_id>\d+)/$', 'mysite.polls.views.detail'),  
+    #url(r'^cliente_edit/(?P<pk>[A-Za-z]+)/$', 'cliente.views.cliente_edit'),
+    
+    url(r'^cliente_edit/(?P<pk>\d+)/$', 'cliente.views.cliente_edit'),
+    
     url(r'^cad_cliente$', 'cliente.views.set_cliente', name='set_cliente'),
     url(r'^produtos$', 'produto.views.produtos', name='produtos'),
     url(r'^vendas_produtos$', 'vendas_produtos.views.vendas', name='vendas'),

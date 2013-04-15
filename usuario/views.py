63175e59ae1d8bad6d44b5195ec7ba5c2c479ddd
Diego Lirio <diegolirio.dl@gmail.com>
2013-04-15 15:52:59 -0300

    ...

--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 from django.shortcuts import render_to_response
 from usuario.models import Usuario
 from usuario.forms import UsuarioForm
@@ -17,29 +19,41 @@ def new(request):
 		if form.is_valid():
 			form.save()
 			#return getAllUsuarios(request)
+			#return HttpResponseRedirect(reverse('usuario.views.getAllUsuarios'))			
+			return HttpResponse('<h4>Usuario Cadastro com sucesso</h4><p><a href="/usuarios">Lista</a></p>')			
 			#return HttpResponseRedirect(reverse('usuario.views.getAllUsuarios'))
-			return HttpResponse('<h4>Usuario Cadastro com sucesso</h4><p><a href="/usuarios">Lista</a></p>')
+			
+			#return render_to_response('usuario_edit.html', 
+			#                          {'form': form, 'msg': u'Usuário cadastro com sucesso!'}, 
+			#                          context_instance=RequestContext(request))
 	else:
 		form = UsuarioForm()
 	return render_to_response('usuario_new.html', {'form': form}, context_instance=RequestContext(request))
 		
 def edit(request, pk):
 	usuario = Usuario.objects.get(pk=pk)
+	msg = ""
 	if request.method == 'POST':
 		#return getAllUsuarios(request)
 		#return HttpResponse('<h4>Usuario Cadastro com sucesso</h4><p><a href="/usuarios">Lista</a></p>')
 		form = UsuarioForm(request.POST, request.FILES, instance=usuario)
 		if form.is_valid():
 			form.save()
-			return HttpResponseRedirect(reverse('usuario.views.getAllUsuarios'))
+			#return HttpResponseRedirect(reverse('usuario.views.getAllUsuarios'))
+			return render_to_response('usuario_edit.html', 
+			                          {'form': form, 'msg': u'Usuário cadastro com sucesso!'}, 
+			                          context_instance=RequestContext(request))			
 	else:
 		form = UsuarioForm(instance=usuario)
-	return render_to_response('usuario_edit.html', {'form': form}, context_instance=RequestContext(request))
+	return render_to_response('usuario_edit.html', {'form': form, 'msg': msg}, context_instance=RequestContext(request))
 	
 def usuario_delete(request, pk):
 	u = Usuario.objects.get(pk=pk)
+	delected = 'N'
 	if request.method == 'POST':
 		u.delete()
-		return getAllUsuarios(request)
-	else:
-		return render_to_response('usuario_delete.html', {'usuario': u}, context_instance=RequestContext(request))
+		delected = 'S'
+		#return getAllUsuarios(request)
+		#return render_to_response('usuario_delete.html', )
+	#else:
+	return render_to_response('usuario_delete.html', {'usuario': u, 'delected': delected}, context_instance=RequestContext(request))

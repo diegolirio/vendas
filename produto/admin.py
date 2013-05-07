09306655fceafebe157895171855a1b30a8d540e
Diego Lirio <diegolirio.dl@gmail.com>
2013-05-07 09:14:28 -0300

    OK

--- produto/admin.py
+++ produto/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from produto.models import Marca
 from produto.models import Produto
+from produto.models import Fornecedor
 
 admin.site.register(Marca)
 admin.site.register(Produto)
+admin.site.register(Fornecedor)

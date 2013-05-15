3f1ac73ede25e2ee3227eb81af473bc0e64d4cfe
Diego Lirio <diegolirio.dl@gmail.com>
2013-05-15 17:58:35 -0300

    OK.... destrinchando form NF

--- vendas_produtos/forms.py
+++ vendas_produtos/forms.py
@@ -1,9 +1,15 @@
+from django import forms
 from django.forms import ModelForm
 from vendas_produtos.models import *
 
+class NFForm(forms.Form):
+	numero = models.IntegerField()   
+	cliente = models.ForeignKey(Cliente) 
+	data = forms.DateField( widget=forms.DateInput(format='d%/%m/%Y'), input_formats=['%d/%m/%Y','%d/%m/%y'])	
+
 class NotaFiscalForm(ModelForm):
 	class Meta:
-		model = NotaFiscal
+		model = NotaFiscal	
 		
 class ItemsForm(ModelForm):
 	class Meta:
